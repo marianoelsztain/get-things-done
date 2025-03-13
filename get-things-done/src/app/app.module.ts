b27b036f1b1bb452049d38fe50a639d19534b347
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2025-03-13 11:05:31 -0300

    fix(app-module): fix task list component name

--- get-things-done/src/app/app.module.ts
+++ get-things-done/src/app/app.module.ts
@@ -3,13 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TakListComponent } from './components/tak-list/tak-list.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
 import { TaskComponent } from './components/task/task.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TakListComponent,
+    TaskListComponent,
     TaskComponent,
   ],
   imports: [
