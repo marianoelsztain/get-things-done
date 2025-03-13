edb61b711477f9c9d81548372c7002971f8860a1
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2025-03-13 11:13:58 -0300

    feat(add-task): empty add task component

--- get-things-done/src/app/app.module.ts
+++ get-things-done/src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskListComponent } from './components/task-list/task-list.component';
 import { TaskComponent } from './components/task/task.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     TaskListComponent,
     TaskComponent,
+    AddTaskComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
