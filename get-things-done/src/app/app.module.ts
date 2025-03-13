9e2f0f1fb86a77e8082af23582248515034905b6
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2025-03-13 10:47:14 -0300

    chore(app): default angular app cleanup

--- get-things-done/src/app/app.module.ts
+++ get-things-done/src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TakListComponent } from './components/tak-list/tak-list.component';
 import { TaskComponent } from './components/task/task.component';
 
 @NgModule({
   declarations: [
+    AppComponent,
+    TakListComponent,
     TaskComponent,
   ],
   imports: [
