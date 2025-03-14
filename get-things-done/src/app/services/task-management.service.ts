1f7ae7af6263def063beb73450998fb3e1fc576f
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2025-03-14 09:42:11 -0300

    refactor(task-management-service): simplified local storage methods, new defaults and add task method

--- get-things-done/src/app/services/task-management.service.ts
+++ get-things-done/src/app/services/task-management.service.ts
@@ -1,27 +1,53 @@
 import { Injectable } from '@angular/core';
 import { Task } from '../models/task';
-import { Observable, Subject } from 'rxjs';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskManagementService {
-  public taskList = new Subject<Task[]>()
+  public taskListSubject = new BehaviorSubject<Task[]>([])
+
+  taskList$ = this.taskListSubject.asObservable();
 
   constructor() { }
 
-  getTaskList(): Observable<Task[]> {
-    return this.taskList.asObservable();
-  }
 
   retrieveTaskListFromStorage() {
-    const taskListString = window.localStorage.getItem('taskList');
-    this.taskList.next(JSON.parse(taskListString));
+    const stringifiedTaskList = window.localStorage.getItem('taskList');
+    return JSON.parse(stringifiedTaskList);
   }
 
-  saveTaskList(listToSave: Task[]) {
-    const taskListStringified = JSON.stringify(listToSave);
-    window.localStorage.setItem('taskList', taskListStringified);
+  updateTaskListState(updatedList: Task[]) {
+    this.taskListSubject.next(updatedList);
   }
 
+  saveTaskListToStorage(listToSave: Task[]) {
+    window.localStorage.setItem('taskList', JSON.stringify(listToSave));
+  }
+
+  addTask(taskTitle: string) {
+    const currentTaskList = this.taskListSubject.value;
+    const newTaskId = currentTaskList.length + 1
+    const newTask = this.createDefaultTask(taskTitle, newTaskId);
+
+    const newTaskList = [...currentTaskList, newTask];
+
+    this.saveTaskListToStorage(newTaskList);
+    this.updateTaskListState(newTaskList);
+  }
+
+  createDefaultTask(title: string, id: number): Task {
+    return {
+      id,
+      title,
+      description: '',
+      completed: false,
+      deleted: false,
+    }
+  }
+
+  setDefaultState() {
+    this.updateTaskListState(this.retrieveTaskListFromStorage());
+  }
 }
